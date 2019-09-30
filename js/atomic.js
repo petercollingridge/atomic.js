64392dfac479cf6f958fe78fe98bc1e968a6b034
Peter Collingridge <peter.collingridge@gmail.com>
2019-09-30 23:02:11 +0100

    Create addParticles method

--- js/atomic.js
+++ js/atomic.js
@@ -1,13 +1,48 @@
 var Atomic = (function () {
-    var Atomic = {
-        makeWorld: function(id, width, height) {
-            var parent = document.getElementById(id);
-            var canvas = document.createElement('canvas');
-            canvas.width = width;
-            canvas.height = height;
-            parent.appendChild(canvas);
-        }
+    // Constants
+    var TAU = 2 * Math.PI;
+
+    // Functions
+    var random = Math.random;
+
+    var INITIAL_SPEED = 1;
+
+    var Particle = function(x, y) {
+        // Position
+        this.x = x;
+        this.y = y;
+        
+        // Velocity
+        var speed = INITIAL_SPEED * random();
+        var angle = TAU * random();
+        this.dx = speed * Math.cos(angle);
+        this.dy = speed * Math.sin(angle);
     };
 
-    return Atomic;
+    function makeWorld(id, width, height) {
+        var parent = document.getElementById(id);
+        var canvas = document.createElement('canvas');
+        canvas.width = width;
+        canvas.height = height;
+        parent.appendChild(canvas);
+
+        var particles = [];
+
+        function addParticles(n) {
+            for (var i = 0; i < n; i++) {
+                var x = width * random();
+                var y = height * random();
+                particles.push(new Particle(x, y));
+            }
+        };
+
+        return {
+            particles: particles,
+            addParticles: addParticles,
+        };
+    }
+
+    return {
+        makeWorld: makeWorld
+    };
 })();
--- js/examples.js
+++ js/examples.js
@@ -1,1 +1,3 @@
 var world = Atomic.makeWorld('world-1', 300, 200);
+world.addParticles(10);
+console.log(world);
